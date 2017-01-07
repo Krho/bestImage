d5d3d86cbb059f121d947001b5a27fe8e3c9fc6c
Caroline Lena <caroline.becker@wikimedia.fr>
2017-01-07 22:44:13 +0100

    Actual script name

--- cgi-bin/cgi.py
+++ cgi-bin/cgi.py
@@ -1,10 +1,10 @@
 /mnt/nfs/labstore-secondary-tools-project/best-image/python/bin/python
 import site
-site.addsitedir("/data/project/tools-dashboard/python/lib/python2.7/site-packages")
+site.addsitedir("/data/project/best-image/python/lib/python2.7/site-packages")
 
 from wsgiref.handlers import CGIHandler
 from app import app
 import os
 
-os.environ['SCRIPT_NAME'] = '/tools-dashboard'
+os.environ['SCRIPT_NAME'] = '/best-image'
 CGIHandler().run(app)
