fd22869e3dd6045cbc294279470486d41e6684ce
C. Becker <carobecker54@gmail.com>
2018-05-18 15:44:34 +0200

    Toolforge deployment

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $CURRENT_DIR/defaults.sh
+
+set -ex
+
+ln -sfn $SOURCE_PATH $APP_PATH
+
+if [ ! -d $VIRTUAL_ENV_PATH ]; then
+    echo "Creating virtual environment..."
+    virtualenv $VIRTUAL_ENV_PATH --system-site-packages
+fi
+
+source $VIRTUAL_ENV_PATH/bin/activate
+
+echo "Installing Python requirements..."
+pip install -r $SOURCE_PATH/requirements.txt
---
+++ bin/deploy-to-toolforge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+user="";
+if [ -n "$1" ]; then
+    user="$1"@
+fi
+ssh "$user"tools-dev.wmflabs.org <<'ENDSSH'
+become visualcategories
+cd bestImage
+
+echo "Pulling changes from Git..."
+git pull
+git log @{1}.. --oneline --reverse -C --no-merges
+
+echo "Updating dependencies..."
+./bin/build.sh
+
+echo "Deploy done."
+echo "Please update the Server Admin Log via IRC:"
+echo "https://webchat.freenode.net/?channels=#wikimedia-labs"
+ENDSSH
